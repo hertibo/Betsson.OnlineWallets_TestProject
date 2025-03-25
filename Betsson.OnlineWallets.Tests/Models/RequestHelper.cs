0867a29d6023ffc49c88b2dfac7abdd93bf40953
Tibor Herczeg <tibor.herczeg@prozenda.com>
2025-03-25 13:25:13 +0100

    configure test environment for tests

---
+++ Betsson.OnlineWallets.Tests/Models/RequestHelper.cs
@@ -0,0 +1,27 @@
+﻿using Betsson.OnlineWallets.Web.Models;
+
+namespace Betsson.OnlineWallets.Tests.Models
+{
+    public class RequestHelper
+    {
+        public const string BalanceEndpoint = "onlinewallet/balance";
+        public const string DepositEndpoint = "onlinewallet/deposit";
+        public const string WithdrawalEndpoint = "onlinewallet/withdraw";
+
+        public static DepositRequest CreateDepositRequest(decimal amount)
+        {
+            return new DepositRequest
+            {
+                Amount = amount
+            };
+        }
+        public static WithdrawalRequest CreateWithdrawalRequest(decimal amount)
+        {
+            return new WithdrawalRequest
+            {
+                Amount = amount
+            };
+        }
+    }
+}
+}
---
+++ Betsson.OnlineWallets.Tests/Models/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using Betsson.OnlineWallets.Web.Models;
+using System.Net.Http.Json;
+
+namespace Betsson.OnlineWallets.Tests.Models
+{
+    public class TestBase
+    {
+        protected readonly HttpClient _client;
+
+        public TestBase()
+        {
+            _client = new HttpClient { BaseAddress = new Uri("http://localhost:3000/") };
+        }
+
+        // Method to get balance in every tests class
+        public async Task<BalanceResponse> GetBalance()
+        {
+            var response = await _client.GetAsync(RequestHelper.BalanceEndpoint);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Failed to get balance. Status Code: {response.StatusCode}");
+            }
+
+            return await response.Content.ReadFromJsonAsync<BalanceResponse>() ?? new BalanceResponse();
+        }
+    }
+}
